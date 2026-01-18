193fd7bba3928c6696c07b1af088b3e6cc8ddf31
1813amit <rajj95842434@gmail.com>
2026-01-18 14:42:20 +0530

    solve

--- Basic-Array/src/Reverse.java
+++ Basic-Array/src/Reverse.java
@@ -24,7 +24,7 @@ public class Reverse {
     }
     static void printArray(int[] arr){
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + "  ");
         }
 
     }
