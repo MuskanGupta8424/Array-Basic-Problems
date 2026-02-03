7bcbb837c7992554fefa9921eb8dc248a96ca011
Muskan Gupta <mg8303655@gmail.com>
2026-02-03 17:55:08 +0530

    solve sorted non-decreasing squared array problem

---
+++ Basic-Array/src/SortedSquaredArray.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SortedSquaredArray {
+//sorted non- decreasing order
+    static int[] sortedSquareArray(int[] arr){
+int i= 0; int j= arr.length-1;
+int n= arr.length;
+int k =0;
+int[] ans =  new int[n];
+
+    while(i<=j){
+   if(Math.abs(arr[i])> Math.abs(arr[j])){
+       ans[k++] = arr[i]*arr[i];
+       i++;
+   }else{
+       ans[k++] = arr[j] * arr[j];
+       j--;
+   }
+    }
+return ans;
+    }
+    static  void reverse(int[] arr){
+        int i=0, j=arr.length-1;
+        while(i<j){
+            int temp =arr[i];
+            arr[i] = arr[j];
+            arr[j] =temp;
+            i++;
+            j--;
+        }
+    }
+    static void printArray(int[] arr){
+        int n= arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter size of an array");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the value of an array ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] ans = sortedSquareArray(arr);
+        reverse(ans);
+        printArray(ans);
+    }
+}
